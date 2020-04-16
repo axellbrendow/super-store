d548d149bf16700036a27ff086faa1a2ceaacccc
axell-brendow <breno.axel@gmail.com>
2020-04-16 01:26:45 -0300

    deploy

---
+++ sw.js
@@ -0,0 +1,123 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "d09b6e2f31666a403177ca21eec92840"
+  },
+  {
+    "url": "main-es2015.06685c43d3bb9d301db5.js",
+    "revision": "3416118cd8b28623157efc50afc3c2e2"
+  },
+  {
+    "url": "main-es5.aa91f758ce1edd2909a0.js",
+    "revision": "ea4a5dec53f139cf1c65a71465655bed"
+  },
+  {
+    "url": "polyfills-es2015.5728f680576ca47e99fe.js",
+    "revision": "359f06bcac366b2f858f56d5c7761d60"
+  },
+  {
+    "url": "polyfills-es5.943113ac054b16d954ae.js",
+    "revision": "053dd45a8680458eed87833bfce7f947"
+  },
+  {
+    "url": "runtime-es2015.858f8dd898b75fe86926.js",
+    "revision": "cd1ce3e306bf57f272364d1cc0249d6e"
+  },
+  {
+    "url": "runtime-es5.741402d1d47331ce975c.js",
+    "revision": "cd1ce3e306bf57f272364d1cc0249d6e"
+  },
+  {
+    "url": "styles.0365c55a8b4afcbc7a3a.css",
+    "revision": "e97de900d7540647e7c3b64765b9e5a6"
+  },
+  {
+    "url": "assets/avatar.png",
+    "revision": "bda9b390221c6fd0e98869cca64cfa25"
+  },
+  {
+    "url": "assets/sda-control.svg",
+    "revision": "cf04c55b497a866e3b5c486f15aa4f7e"
+  },
+  {
+    "url": "assets/super-store.svg",
+    "revision": "8df0df15b5f843c322ac92e58e34fcce"
+  },
+  {
+    "url": "assets/games/captain.jpg",
+    "revision": "592fadaed0f6733746e9b80cf22afd54"
+  },
+  {
+    "url": "assets/games/cdino.png",
+    "revision": "20633b39dd474e3dc772140c395e94e1"
+  },
+  {
+    "url": "assets/games/final-fight.jpg",
+    "revision": "09df5c429e9f36d36cd1fd71d650aee0"
+  },
+  {
+    "url": "assets/games/fzero.jpg",
+    "revision": "32f24b1f5432b269b22c006355c8b1b7"
+  },
+  {
+    "url": "assets/games/mario.jpg",
+    "revision": "8696e995a1b0c46c204100681cd226dc"
+  },
+  {
+    "url": "assets/games/megaman.png",
+    "revision": "d946b148aa9b3fb6315637cf3519cc8c"
+  },
+  {
+    "url": "assets/games/metal-gear.jpg",
+    "revision": "565b0089d685460efa8a72c9f9906f3a"
+  },
+  {
+    "url": "assets/games/mortal-kombat.jpg",
+    "revision": "df65bfd138fcd7c1eba21a3f1956f726"
+  },
+  {
+    "url": "assets/games/street-fighter.jpg",
+    "revision": "ededc61895683e62a556814cd86015ff"
+  },
+  {
+    "url": "assets/games/topgear.jpg",
+    "revision": "0559335f257af6d2a20d09986a43974b"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "c13c50e6ae7e3765e05a736bde790b76"
+  },
+  {
+    "url": "?utm_source=pwa",
+    "revision": "40857339e133d24b7bdb860c01dcbe98"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"));
